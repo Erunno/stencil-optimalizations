7e791c68d162fc35b871ccf4ae5215891bfa7b73
Matyáš Brabec <matyas.brabec@seznam.cz>
2025-02-05 17:22:00 +0100

    implemented full packed rows

--- src/algorithms/_shared/bitwise/bitwise-ops/fully-packed-rows.cuh
+++ src/algorithms/_shared/bitwise/bitwise-ops/fully-packed-rows.cuh
@@ -18,69 +18,47 @@ struct FullyPackedWithVectorOperationsImplementation {
     template <int c>
     using constant_2 = Consts<word_type, c, 2>;
 
-    constexpr static std::size_t BITS_PER_CELL = WastefulRows<word_type>::BITS_PER_CELL;
-    constexpr static std::size_t BITS = WastefulRows<word_type>::BITS;
-    constexpr static std::size_t CELLS_PER_WORD = WastefulRows<word_type>::CELLS_PER_WORD;
-
-    constexpr static word_type CELL_MASK = (static_cast<word_type>(1) << BITS_PER_CELL) - 1;
+    constexpr static std::size_t BITS = sizeof(word_type) * 8;
+    constexpr static word_type mask_01 = constant_2<0b01>::expanded;
+    constexpr static word_type mask_0001 = constant_4<0b0001>::expanded;
+    constexpr static word_type mask_0011 = constant_4<0b0011>::expanded;
+    constexpr static word_type mask_1100 = constant_4<0b1100>::expanded;
 
     static __host__ __device__ __forceinline__  word_type compute_center_word(
         word_type lt, word_type ct, word_type rt, 
         word_type lc, word_type cc, word_type rc,
         word_type lb, word_type cb, word_type rb) {
 
-        constexpr word_type mask_01 = constant_2<0b01>::expanded;
-        constexpr word_type mask_10 = constant_2<0b10>::expanded;
-
         word_type neighborhoods_01 = (ct & mask_01) + (cc & mask_01) + (cb & mask_01);
-        word_type neighborhoods_10 = ((ct & mask_10) >> 1) + ((cc & mask_10) >> 1) + ((cb & mask_10) >> 1);
+        word_type neighborhoods_10 = ((ct >> 1) & mask_01) + ((cc >> 1) & mask_01) + ((cb >> 1) & mask_01);
 
-        word_type neighborhoods_01_0011 = neighborhoods_01 & constant_4<0b0011>::expanded;
-        word_type neighborhoods_01_1100 = neighborhoods_01 & constant_4<0b1100>::expanded >> 2;
+        word_type left_neighborhood = (lt >> (BITS - 1)) + (lc >> (BITS - 1)) + (lb >> (BITS - 1));
+        word_type right_neighborhood = ((rt & mask_0001) + (rc & mask_0001) + (rb & mask_0001)) << (BITS - 4);
+    
+        word_type neighborhoods_01_0011 = neighborhoods_01 & mask_0011;
+        word_type neighborhoods_01_1100 = neighborhoods_01 & mask_1100;
+        word_type neighborhoods_10_0011 = neighborhoods_10 & mask_0011;
+        word_type neighborhoods_10_1100 = neighborhoods_10 & mask_1100;
 
-        word_type neighborhoods_10_0011 = neighborhoods_10 & constant_4<0b0011>::expanded;
-        word_type neighborhoods_10_1100 = neighborhoods_10 & constant_4<0b1100>::expanded >> 2;
+        word_type complete_neighborhoods_01_0011 =  neighborhoods_01_0011       +  neighborhoods_10_0011       + (neighborhoods_10_1100 << 2) + left_neighborhood;
+        word_type complete_neighborhoods_01_1100 = (neighborhoods_01_1100 >> 2) + (neighborhoods_10_1100 >> 2) +  neighborhoods_10_0011;
+        word_type complete_neighborhoods_10_0011 =  neighborhoods_10_0011       +  neighborhoods_01_0011       + (neighborhoods_01_1100 >> 2);
+        word_type complete_neighborhoods_10_1100 = (neighborhoods_10_1100 >> 2) + (neighborhoods_01_1100 >> 2) + (neighborhoods_01_0011 >> 4) + right_neighborhood;
 
-        word_type complete_neighborhoods_01_0011 = neighborhoods_01_0011 + neighborhoods_10_0011 + (neighborhoods_10_1100 << 2);
-        word_type complete_neighborhoods_01_1100 = neighborhoods_01_1100 + neighborhoods_10_1100 + (neighborhoods_01_0011 << 2);
-
+        word_type result_01_0011 = GOL( cc       & mask_0001, complete_neighborhoods_01_0011);
+        word_type result_01_1100 = GOL((cc >> 2) & mask_0001, complete_neighborhoods_01_1100);
+        word_type result_10_0011 = GOL((cc >> 1) & mask_0001, complete_neighborhoods_10_0011);
+        word_type result_10_1100 = GOL((cc >> 3) & mask_0001, complete_neighborhoods_10_1100);
         
-
-
-
-        // word_type right_neighborhood =
-        //     (rt & 1 << (BITS - 2)) +
-        //     (rc & 1 << (BITS - 2)) +
-        //     (rb & 1 << (BITS - 2));
- 
-        // word_type left_neighborhood =
-        //     (lt >> (BITS - 1)) +
-        //     (lc >> (BITS - 1)) +
-        //     (lb >> (BITS - 1));
-
-        // neighborhoods_01 += right_neighborhood;
-        // neighborhoods_10 += left_neighborhood;
-
-        return constant_4<0b1111>::expanded;
+        return result_01_0011 | (result_01_1100 << 2) | (result_10_0011 << 1) | (result_10_1100 << 3);
     }
 
-
-    static __host__ __device__ __forceinline__  word_type compute_having_2_bits(
-        word_type lt, word_type ct, word_type rt, 
-        word_type lc, word_type cc, word_type rc,
-        word_type lb, word_type cb, word_type rb) {
-
-        word_type neighborhoods = ct + cc + cb;
-        neighborhoods += (neighborhoods >> BITS_PER_CELL) + (neighborhoods << BITS_PER_CELL);
-
-        word_type right_neighborhoods = (rt + rc + rb) << (BITS - BITS_PER_CELL);
-        word_type left_neighborhoods = (lt + lc + lb) >> (BITS - BITS_PER_CELL);
-
-        neighborhoods += right_neighborhoods + left_neighborhoods;
+    static __host__ __device__ __forceinline__ word_type GOL(
+        word_type alive, word_type neighborhood) {
         
-        neighborhoods -= cc;
+        neighborhood -= alive;
 
-        word_type is_3 = (cc | neighborhoods) ^ constant_4<0b1100>::expanded;
+        word_type is_3 = (alive | neighborhood) ^ constant_4<0b1100>::expanded;
         is_3 = (is_3 >> 2) & is_3;
         is_3 = (is_3 >> 1) & is_3;
 
