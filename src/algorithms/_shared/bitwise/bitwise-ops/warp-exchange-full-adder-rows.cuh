0c9bf83642f4c11bf9d20ca6f67e5d8ff77cd70b
Matyáš Brabec <matyas.brabec@seznam.cz>
2025-04-09 16:15:39 +0200

    33 ops implementation

--- src/algorithms/_shared/bitwise/bitwise-ops/warp-exchange-full-adder-rows.cuh
+++ src/algorithms/_shared/bitwise/bitwise-ops/warp-exchange-full-adder-rows.cuh
@@ -21,126 +21,33 @@ struct WarpExchangeFullAdderOnRows {
         word_type cc,
         word_type cb) {
 
-        word_type _0_center, _1_center;
-        add_three_bits(
-            ct, cc, cb,
-            _0_center, _1_center); // 5 ops
+        const word_type _0_center_only_neighbors = ct ^ cb;
+        const word_type _1_center_only_neighbors = ct & cb;
 
-        word_type _0_right = shift_val_within_warp<ShiftDirection::LEFT>(_0_center);
-        word_type _1_right = shift_val_within_warp<ShiftDirection::LEFT>(_1_center);
+        const word_type _0_center_full_column = _0_center_only_neighbors ^ cc;
+        const word_type _1_center_full_column = _1_center_only_neighbors | (_0_center_only_neighbors & cc);
 
-        word_type _0_left = shift_val_within_warp<ShiftDirection::RIGHT>(_0_center);
-        word_type _1_left = shift_val_within_warp<ShiftDirection::RIGHT>(_1_center);
+        const word_type _0_right = shift_val_within_warp<ShiftDirection::LEFT>(_0_center_full_column);
+        const word_type _1_right = shift_val_within_warp<ShiftDirection::LEFT>(_1_center_full_column);
 
-        word_type _0_shifted_left = (_0_center << 1) | (_0_left >> (BITS - 1)); // 3 ops
-        word_type _1_shifted_left = (_1_center << 1) | (_1_left >> (BITS - 1)); // 3 ops
+        const word_type _0_left = shift_val_within_warp<ShiftDirection::RIGHT>(_0_center_full_column);
+        const word_type _1_left = shift_val_within_warp<ShiftDirection::RIGHT>(_1_center_full_column);
 
-        word_type _0_shifted_right = (_0_center >> 1) | (_0_right << (BITS - 1)); // 3 ops
-        word_type _1_shifted_right = (_1_center >> 1) | (_1_right << (BITS - 1)); // 3 ops
+        const word_type _0_shifted_left = (_0_center_full_column << 1) | (_0_left >> (BITS - 1)); // 3 ops
+        const word_type _1_shifted_left = (_1_center_full_column << 1) | (_1_left >> (BITS - 1)); // 3 ops
+
+        const word_type _0_shifted_right = (_0_center_full_column >> 1) | (_0_right << (BITS - 1)); // 3 ops
+        const word_type _1_shifted_right = (_1_center_full_column >> 1) | (_1_right << (BITS - 1)); // 3 ops
 
         // partial = 17 ops
 
-        word_type r_0 = ct ^ cb, r_1 = ct & cb, r_2 = 0; // 2 ops -- ! can be for free (computed in add_three_bits)
-
-        add_two(
+        return from_7_bits_to_result(
             _0_shifted_left, _1_shifted_left,
-            r_0, r_1, r_2); // 8 ops
-        add_two(
-            _0_shifted_right, _1_shifted_right,
-            r_0, r_1, r_2); // 8 ops
+            _0_center_only_neighbors, _1_center_only_neighbors,
+            _0_shifted_right, _1_shifted_right,            
+            cc); // 16 ops
 
-        // word_type r_0 = 0, r_1 = 0, r_2 = 0;
-        // add_three_two_bits(
-        //     _0_shifted_left, _1_shifted_left,
-        //     _0_shifted_right, _1_shifted_right,
-        //     _0_center, _1_center,
-        //     r_0, r_1, r_2); // 13 ops
-            
-        // return GOL(cc, r_0, r_1, r_2); // 9 ops
-        return GOL_8(cc, r_0, r_1, r_2); // 4 ops
-
-        // total 37 ops
-    }
-
-    static __device__ __forceinline__ void add_one(word_type a, word_type& _0, word_type& _1) {
-        word_type r_0 = a ^ _0;
-
-        word_type _0_carry = a & _0;
-        word_type r_1 = _0_carry ^ _1;
-
-        _0 = r_0;
-        _1 = r_1;
-    }
-
-    static __device__ __forceinline__ void add_three_bits(
-        word_type a, word_type b, word_type c,
-        word_type& _0, word_type& _1) {
-
-        word_type axb = a ^ b;
-
-        _0 = axb ^ c;
-        _1 = (a & b) | (axb & c);
-    }
-
-    static __device__ __forceinline__ void add_two(
-        word_type _0a, word_type _1a,
-        word_type& _0, word_type& _1, word_type& _2
-    ){
-        word_type _1_xor_1a = _1 ^ _1a;
-
-        word_type r_0 = _0a ^ _0;
-
-        word_type _0_carry = _0a & _0;
-        word_type r_1 = _1_xor_1a ^ _0_carry;
-
-        word_type _1_carry = (_1 & _1a) | (_0_carry & (_1_xor_1a));
-
-        _0 = r_0;
-        _1 = r_1;
-        _2 = _2 ^ _1_carry;
-    }
-
-    static __device__ __forceinline__ void add_three_two_bits(
-    word_type a0, word_type a1, 
-    word_type b0, word_type b1, 
-    word_type c0, word_type c1,
-    word_type& s2, word_type& s1, word_type& s0) {
-    
-    // Compute LSB sum and carry
-    word_type a0xb0 = a0 ^ b0;
-    s0 = a0xb0 ^ c0;
-    word_type carry = (a0 & b0) | (a0xb0 & c0);
-
-    // Compute intermediate values for MSB calculations
-    word_type a1xb1 = a1 ^ b1;
-    word_type sum_msb = a1xb1 ^ c1;
-    word_type carry_msb = (a1 & b1) | (a1xb1 & c1);
-
-    // Compute final s1 and s2
-    s1 = sum_msb ^ carry;
-    s2 = carry_msb | (carry & sum_msb);
-}
-
-    constexpr static word_type ones = ~static_cast<word_type>(0);
-    constexpr static word_type zeros = 0;
-
-    static __device__ __forceinline__ word_type GOL(
-        word_type alive, word_type _0, word_type _1, word_type _2) {
-
-        auto is_3 = _0 & _1 & (_2 ^ ones);
-        auto is_4 = (_0 ^ ones) & (_1 ^ ones) & _2;
-
-        return is_3 | (is_4 & alive);
-
-        // total 9 ops
-    }
-
-    static __device__ __forceinline__ word_type GOL_8(
-        word_type alive, word_type _0, word_type _1, word_type _2) {
-
-        return (_0 | alive) & _1 & (_2 ^ ones);
-
-        // total 4 ops
+        // total = 33 ops
     }
 
     static __device__ __forceinline__ word_type from_7_bits_to_result(
