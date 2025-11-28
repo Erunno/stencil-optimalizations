68a486301a1b275c08c310ef8ed60ad1ed6879a2
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2025-11-28 18:25:07 +0100

    improve the lop3 definition; support 64 bits

--- src/algorithms/_shared/bitwise/bitwise-ops/warp-exchange-full-adder-rows.cuh
+++ src/algorithms/_shared/bitwise/bitwise-ops/warp-exchange-full-adder-rows.cuh
@@ -6,14 +6,58 @@
 #include "../bit_modes.hpp"
 #include <cuda_runtime.h>
 #include "../../cuda-helpers/shift.cuh"
+#include <algorithm>
+#include <ranges>
+#include <type_traits>
 
 namespace algorithms {
 
+// wrapper for truth rows encoded as unsigned int that supports only bitwise OR operation
+template<unsigned int data>
+requires  (data <= 0xFF) && (data >= 0)
+struct lut3 : std::integral_constant<unsigned int, data> {
+    template<unsigned int rhs_data>
+    __host__ __device__ constexpr auto operator|(lut3<rhs_data>) -> lut3<data | rhs_data> {
+        return {};
+    }
+};
 
+template<char... row>
+constexpr auto operator""_tr() {
+    constexpr unsigned int ta = 0xF0;
+    constexpr unsigned int tb = 0xCC;
+    constexpr unsigned int tc = 0xAA;
+
+    constexpr char arr[] = {row...};
+    static_assert(sizeof(arr) == 3, "Truth row must be exactly 3 characters long.");
+    static_assert(std::all_of(std::begin(arr), std::end(arr), [](char c){ return c == '0' || c == '1'; }), "Truth row can only contain '0' and '1' characters.");
+
+    constexpr unsigned int immLut = (arr[0] == '1' ? ta : ~ta) &
+                                    (arr[1] == '1' ? tb : ~tb) &
+                                    (arr[2] == '1' ? tc : ~tc) & 0xFF;
+
+    return lut3<immLut>{};
+}
+
+template<typename word_type, unsigned int immLut>
+requires (sizeof(word_type) == 4)
+__device__ __forceinline__
+word_type run_lop3_gate(
+    const word_type in1,
+    const word_type in2,
+    const word_type in3,
+    lut3<immLut>) {
+    word_type out;
+    asm (
+        "lop3.b32 %0, %1, %2, %3, %4;"
+        : "=r"(out)
+        : "r"(in1), "r"(in2), "r"(in3), "n"(immLut)
+    );
+    return out;
+}
 
 template <typename word_type>
 struct WarpExchangeFullAdderOnRows {
-
     constexpr static std::size_t BITS = sizeof(word_type) * 8;
 
     static __device__ __forceinline__  word_type compute_center_word(
@@ -53,154 +97,82 @@ struct WarpExchangeFullAdderOnRows {
     static __device__ __forceinline__ word_type from_7_bits_to_result(
         word_type i1, word_type i2, word_type i3, word_type i4,
         word_type i5, word_type i6, word_type i7) {
-        
+
+        static_assert(BITS == 32 || BITS == 64, "Only 32-bit and 64-bit word types are supported.");
+
         // .model spec
         // .inputs 1 2 3 4 5 6 7
-        // .outputs 8
-        // .names 1 3 5 22
-        // 010 1
-        // 101 1
-        // .names 3 4 22 11
+        // .outputs 12
+        // .names 1 3 5 18
         // 001 1
+        // 010 1
+        // 100 1
+        // 111 1
+        // .names 1 5 18 8
         // 010 1
         // 100 1
         // 110 1
         // 111 1
-        // .names 2 6 11 12
-        // 001 1
+        // .names 4 8 18 11
         // 010 1
-        // 100 1
-        // .names 1 4 7 21
-        // 001 1
         // 011 1
         // 100 1
+        // 101 1
         // 110 1
         // 111 1
-        // .names 3 5 21 9
-        // 010 1
-        // 011 1
-        // 100 1
-        // 101 1
-        // .names 1 21 9 10
+        // .names 2 6 11 9
         // 001 1
         // 010 1
-        // 011 1
         // 100 1
+        // .names 1 4 7 10
+        // 001 1
+        // 011 1
+        // 101 1
+        // .names 9 18 10 12
         // 101 1
         // 110 1
-        // .names 10 12 23
-        // 11 1
-        // .names 23 8
-        // 1 1
+        // 111 1
         // .end
 
-        
-        
+        constexpr auto compute_32 = []<typename T>(T i1, T i2, T i3, T i4, T i5, T i6, T i7) -> T {
+            // (.names == input1 input2 input3 -> output gate)
+            // .names 1 3 5 18
+            // (truth table; ignore the last column)
+            // 001 1
+            // 010 1
+            // 100 1
+            // 111 1
+            T gate_18 = run_lop3_gate(i1, i3, i5, 001_tr | 010_tr | 100_tr | 111_tr);
+
+            T gate_8 = run_lop3_gate(i1, i5, gate_18, 010_tr | 100_tr | 110_tr | 111_tr);
+            T gate_11 = run_lop3_gate(i4, gate_8, gate_18, 010_tr | 011_tr | 100_tr | 101_tr | 110_tr | 111_tr);
+            T gate_9 = run_lop3_gate(i2, i6, gate_11, 001_tr | 010_tr | 100_tr);
+            T gate_10 = run_lop3_gate(i1, i4, i7, 001_tr | 011_tr | 101_tr);
+            T gate_12 = run_lop3_gate(gate_9, gate_18, gate_10, 101_tr | 110_tr | 111_tr);
+            return gate_12;
+        };
+
         if constexpr (BITS == 32) {
-            constexpr unsigned int ta = 0xF0;
-            constexpr unsigned int tb = 0xCC;
-            constexpr unsigned int tc = 0xAA;
-
-            // immLut has to be given as an immediate value
-
-            // gate_22
-            constexpr unsigned int immLut_22 = (~ta & tb & ~tc) | (ta & ~tb & tc);
-            word_type gate_22;
-            asm (
-                "lop3.b32 %0, %1, %2, %3, 0x24;"
-                : "=r"(gate_22)
-                : "r"(i1), "r"(i3), "r"(i5)
-            );
-            static_assert(immLut_22 == 0x24u, "LUT mismatch");
-
-            // gate_11
-            constexpr unsigned int immLut_11 = (~ta & ~tb & tc) | (~ta & tb & ~tc) | (ta & ~tb & ~tc) | (ta & tb & ~tc) | (ta & tb & tc);
-            word_type gate_11;
-            asm (
-                "lop3.b32 %0, %1, %2, %3, 0xd6;"
-                : "=r"(gate_11)
-                : "r"(i3), "r"(i4), "r"(gate_22)
-            );
-            static_assert(immLut_11 == 0xd6u, "LUT mismatch");
-
-            // gate_12
-            constexpr unsigned int immLut_12 = (~ta & ~tb & tc) | (~ta & tb & ~tc) | (ta & ~tb & ~tc);
-            word_type gate_12;
-            asm (
-                "lop3.b32 %0, %1, %2, %3, 0x16;"
-                : "=r"(gate_12)
-                : "r"(i2), "r"(i6), "r"(gate_11)
-            );
-            static_assert(immLut_12 == 0x16u, "LUT mismatch");
-
-            // gate_21
-            constexpr unsigned int immLut_21 = (~ta & ~tb & tc) | (~ta & tb & tc) | (ta & ~tb & ~tc) | (ta & tb & ~tc) | (ta & tb & tc);
-            word_type gate_21;
-            asm (
-                "lop3.b32 %0, %1, %2, %3, 0xda;"
-                : "=r"(gate_21)
-                : "r"(i1), "r"(i4), "r"(i7)
-            );
-            static_assert(immLut_21 == 0xdau, "LUT mismatch");
-
-            // gate_9
-            constexpr unsigned int immLut_9 = (~ta & tb & ~tc) | (~ta & tb & tc) | (ta & ~tb & ~tc) | (ta & ~tb & tc);
-            word_type gate_9;
-            asm (
-                "lop3.b32 %0, %1, %2, %3, 0x3c;"
-                : "=r"(gate_9)
-                : "r"(i3), "r"(i5), "r"(gate_21)
-            );
-            static_assert(immLut_9 == 0x3cu, "LUT mismatch");
-
-            // gate_10
-            constexpr unsigned int immLut_10 = (~ta & ~tb & tc) | (~ta & tb & ~tc) | (~ta & tb & tc) | (ta & ~tb & ~tc) | (ta & ~tb & tc) | (ta & tb & ~tc);
-            word_type gate_10;
-            asm (
-                "lop3.b32 %0, %1, %2, %3, 0x7e;"
-                : "=r"(gate_10)
-                : "r"(i1), "r"(gate_21), "r"(gate_9)
-            );
-            static_assert(immLut_10 == 0x7eu, "LUT mismatch");
-
-            // gate_23
-            word_type gate_23 = gate_10 & gate_12;
-
-            // output
-            return gate_23;
-
-        } else {
-            // Hidden layer 1
-            const word_type h1_0 = i2 | i6;
-            const word_type h1_1 = i7;
-            const word_type h1_2 = i1 & i4;
-            const word_type h1_3 = i3 | i4;
-            const word_type h1_4 = i1;
-            const word_type h1_5 = i2 & i6;
-            const word_type h1_6 = i3 ^ i5;
-            const word_type h1_7 = i1 ^ i3;
-        
-            // Hidden layer 2
-            const word_type h2_0 = h1_1;
-            const word_type h2_1 = h1_4 ^ h1_6;
-            const word_type h2_2 = h1_6 & h1_7;
-            const word_type h2_3 = h1_5;
-            const word_type h2_4 = h1_0 ^ h1_3;
-            const word_type h2_5 = h1_0 & h1_2;
-        
-            // Hidden layer 3
-            const word_type h3_0 = h2_3 | h2_5;
-            const word_type h3_1 = h2_0 | h2_1;
-            const word_type h3_2 = h2_2 ^ h2_4;
-        
-            // Hidden layer 4
-            const word_type h4_0 = h3_0 ^ h3_2;
-            const word_type h4_1 = h3_1 & h3_2;
-        
-            // Output layer
-            const word_type o1 = h4_0 & h4_1;
-
-            return o1;
+            return compute_32(i1, i2, i3, i4, i5, i6, i7);
+        } else if constexpr (BITS == 64) {
+            return compute_32(
+                static_cast<uint32_t>(i1),
+                static_cast<uint32_t>(i2),
+                static_cast<uint32_t>(i3),
+                static_cast<uint32_t>(i4),
+                static_cast<uint32_t>(i5),
+                static_cast<uint32_t>(i6),
+                static_cast<uint32_t>(i7)
+            ) |
+            (static_cast<uint64_t>(compute_32(
+                static_cast<uint32_t>(i1 >> 32),
+                static_cast<uint32_t>(i2 >> 32),
+                static_cast<uint32_t>(i3 >> 32),
+                static_cast<uint32_t>(i4 >> 32),
+                static_cast<uint32_t>(i5 >> 32),
+                static_cast<uint32_t>(i6 >> 32),
+                static_cast<uint32_t>(i7 >> 32)
+            )) << 32);
         }
     }
 };
